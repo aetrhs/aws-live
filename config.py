c3e8a564ad861382ee071156ae8d8f7bacf80607
aetrhs <167169314+aetrhs@users.noreply.github.com>
2024-04-16 14:46:13 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-2.c5uums4uos0u.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Aacs3253"
+customdb = "employee"
+custombucket = "nurulhuda-employee"
+customregion = "us-east-1"
+
